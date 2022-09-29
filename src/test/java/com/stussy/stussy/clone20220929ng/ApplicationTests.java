cf13a7474ee764d4a6fe7f04c7426b0e0d8f2bbd
nage24 <d0190466@kowon.dongseo.ac.kr>
2022-09-29 09:22:38 +0900

    2022-09-29 stussyClone Project spring initializr

---
+++ src/test/java/com/stussy/stussy/clone20220929ng/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stussy.stussy.clone20220929ng;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
