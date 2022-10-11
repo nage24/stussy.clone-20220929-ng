d0125f3255a19a24a2e57f9cfd0f39a9ea7ba826
nage24 <n0rkjdx0@naver.com>
2022-10-11 22:12:49 +0900

    2022-10-11 (1) product add page 이미지 업로드

--- src/main/resources/static/js/product_add.js
+++ src/main/resources/static/js/product_add.js
@@ -1,59 +1,57 @@
 const fileAddButton = document.querySelector(".add-button");
-const fileInput = document.querySelector(".file-input")
+const fileInput = document.querySelector(".file-input");
+const submitButton = document.querySelector(".submit-button");
 
 let productImageFiles = new Array();
-
 
 fileAddButton.onclick = () => {
     fileInput.click();
 }
 
 fileInput.onchange = () => {
-    const formData = new FormData().document.querySelector("form");
+    const formData = new FormData(document.querySelector("form"));
 
-    formData.forEach((value)  => {
+    formData.forEach((value) => {
         if(value.size != 0) {
             productImageFiles.push(value);
             console.log(productImageFiles);
-
             getImagePreview();
+            fileInput.value = null;
         }
-        // console.log("value: " + value);
-        // console.log("key: " + key);
     });
 }
 
 function getImagePreview() {
-// img 정보 배열
     const productImages = document.querySelector(".product-images");
 
+    productImages.innerHTML = "";
 
-    const reader = new FileReader();
-    reader.onload = (e) => {
-        productImages.innerHTML = "
+
+    productImageFiles.forEach((file, i) => {
+        const reader = new FileReader();
+
+        reader.onload = (e) => {
+            productImages.innerHTML += `
                 <div class="img-box">
                     <i class="fa-solid fa-xmark"></i>
                     <img class="product-img" src="${e.target.result}">
                 </div>
-                ";
+            `;
 
             const deleteButton = document.querySelectorAll(".fa-xmark");
             deleteButton.forEach((xbutton, index) => {
                 xbutton.onclick = () => {
                     if(confirm("상품 이미지를 지우시겠습니까?")) {
                         productImageFiles.splice(index, 1);
+                        console.log(productImageFiles);
                         getImagePreview();
                     }
                 };
-            });
-    }
+            })
+        }
+
+        setTimeout(reader.readAsDataURL(file), i * 100);
 
 
-    productImages.forEach(file => {
-        reader.readAsDataURL(file);
-    }
-
-
-
-    }
+    });
 }
